2d5b127fdbac50d65abb661a798c8607d723b98c
Ben Cook <808@holbertonschool.com>
2019-11-18 21:31:08 -0800

    attempt first advanced task

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+a = ARGV[0].scan(/(?<=\[from:)(?:(?!\]).)*(?=\])/).join
+b = ARGV[0].scan(/(?<=\[to:)(?:(?!\]).)*(?=\])/).join
+c = ARGV[0].scan(/(?<=\[flags:)(?:(?!\]).)*(?=\])/).join
+puts a+','+b+','+c
