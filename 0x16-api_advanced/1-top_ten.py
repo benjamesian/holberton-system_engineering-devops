dee8d2f80090087bc4bc4d03ad3527c6aa7a5868
Ben Cook <808@holbertonschool.com>
2020-02-06 12:38:20 -0800

    fix style

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,9 +12,9 @@ RAW_URL = 'http://reddit.com/r/{:s}/hot.json'
 def top_ten(subreddit):
     """Get the titles of the top ten hottest posts on a subreddit."""
     headers = {'User-agent': 'py3'}
-    resp = get(RAW_URL.format(subreddit), 
-                   headers=headers,
-                   allow_redirects=False)
+    resp = get(RAW_URL.format(subreddit),
+               headers=headers,
+               allow_redirects=False)
     hot_posts = resp.json().get('data', {}).get('children', {})
     if resp.status_code != 200 or not hot_posts:
         print('None')
