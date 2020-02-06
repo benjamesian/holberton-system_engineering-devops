7e62ca78d99548901c93e4626a66b5facb687929
Ben Cook <808@holbertonschool.com>
2020-02-06 12:36:52 -0800

    prevent redirects

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,7 +12,10 @@ RAW_URL = 'http://reddit.com/r/{:s}/hot.json'
 def top_ten(subreddit):
     """Get the titles of the top ten hottest posts on a subreddit."""
     headers = {'User-agent': 'py3'}
-    resp = request('GET', RAW_URL.format(subreddit), headers=headers)
+    resp = request('GET',
+                   RAW_URL.format(subreddit), 
+                   headers=headers,
+                   allow_redirects=False)
     hot_posts = resp.json().get('data', {}).get('children', {})
     if resp.status_code != 200 or not hot_posts:
         print('None')
