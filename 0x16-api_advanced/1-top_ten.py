6c4329814c78d68407340c88407d9a6c37755d64
Ben Cook <808@holbertonschool.com>
2020-02-06 12:40:00 -0800

    use query params to limit results

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,11 +12,13 @@ RAW_URL = 'http://reddit.com/r/{:s}/hot.json'
 def top_ten(subreddit):
     """Get the titles of the top ten hottest posts on a subreddit."""
     headers = {'User-agent': 'py3'}
+    params = {'limit': 10}
     resp = get(RAW_URL.format(subreddit),
                headers=headers,
+               params=params,
                allow_redirects=False)
     hot_posts = resp.json().get('data', {}).get('children', {})
     if resp.status_code != 200 or not hot_posts:
         print('None')
-    for post in hot_posts[:10]:
+    for post in hot_posts:
         print(post.get('data', {}).get('title'))
