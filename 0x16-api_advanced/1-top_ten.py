e0b5acb490f0224c3a830b3b14ddf50edf6224b1
Ben Cook <808@holbertonschool.com>
2020-02-06 12:51:49 -0800

    fix spelling, not sure what is missing

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,15 +10,15 @@ RAW_URL = 'http://reddit.com/r/{:s}/hot.json'
 
 def top_ten(subreddit):
     """Get the titles of the top ten hottest posts on a subreddit."""
-    headers = {'User-agent': 'py3'}
+    headers = {'User-Agent': 'py3'}
     params = {'limit': 10}
     resp = requests.get(RAW_URL.format(subreddit),
                         headers=headers,
                         params=params,
-                        allow_redirects=False)
-    if resp.status_code != 200:
+                        allow_redirects=False,
+                        timeout=10)
+    if resp.status_code == 200:
+        for post in resp.json().get('data', {}).get('children', []):
+            print(post.get('data', {}).get('title'))
+    else:
         print('None')
-        return
-    hot_posts = resp.json().get('data', {}).get('children', [])
-    for post in hot_posts:
-        print(post.get('data', {}).get('title'))
